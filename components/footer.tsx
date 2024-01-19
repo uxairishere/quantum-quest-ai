d60b5126192539c587a996acf8bb0cb7e7114033
uxairishere <uzaaaaaair@gmail.com>
2024-01-20 03:29:55 +0500

    feat(footer): created footer

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Twitter } from 'lucide-react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-8 px-4 text-center">
+      <h1>My Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
