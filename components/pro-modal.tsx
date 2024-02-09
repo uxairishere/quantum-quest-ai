bce4e6637b73ae68deae680f3aa72352d66bf2f1
uxairishere <uzaaaaaair@gmail.com>
2024-02-10 01:03:03 +0500

    feat(stripe): integrated stripe and webhook

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -7,6 +7,8 @@ import { Check, Code, ImageIcon, MessageSquare, Music, VideoIcon, Zap } from "lu
 import { Card } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import axios from "axios";
+import { useState } from "react";
 
 const tools = [
     {
@@ -48,7 +50,20 @@ const tools = [
 
 export const ProModal = () => {
 
-    const proModal = useProModal()
+    const proModal = useProModal();
+    const [loading, setLoading] = useState(false);
+    const onSubscripe = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.get("/api/stripe");
+
+            window.location.href = response.data.url;
+        } catch (error) {
+            console.log(error, "STRIPE_CLIENT_ERROR");
+        } finally {
+            setLoading(false)
+        }
+    }
 
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -83,6 +98,7 @@ export const ProModal = () => {
                         size="lg"
                         variant="premium"
                         className="w-full"
+                        onClick={() => onSubscripe()}
                     >
                         Upgrade
                         <Zap className="w-4 h-4 ml-2 fill-white" />
